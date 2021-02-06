e156801291570db90c4a79147b03c438aad53d23
Davidass <56369703+Davidass@users.noreply.github.com>
2021-02-06 23:46:36 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import App from 'components/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
