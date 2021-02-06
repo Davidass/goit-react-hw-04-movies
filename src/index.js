2fcfe6b84426e317203207ab05082db57ef669f0
Davidass <gidrig@gmail.com>
2021-02-07 01:17:20 +0200

    React BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
-import App from 'components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
